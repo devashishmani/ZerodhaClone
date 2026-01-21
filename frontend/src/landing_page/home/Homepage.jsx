028acf874cfc49576c889b3c4dd27ed1b64c4a8c
Devashish Mani <devashishmani593@gmail.com>
2026-01-21 23:25:16 +0530

    Initial commit - Zerodha Clone frontend

---
+++ frontend/src/landing_page/home/Homepage.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Hero from './Hero'
+import Award from './Award'
+import Stats from './Stats'
+import Pricing from './Pricing'
+import Education from './Education'
+import Navbar from '../Navbar'
+import Openaccount from '../OpenAccount'
+import Footer from '../Footer'
+
+function Homepage() {
+  return (
+    <>
+      
+        <Hero />
+        <Award />
+        <Stats />
+        <Pricing />
+        <Education />
+        <Openaccount />
+        <Footer />
+
+
+
+    </>
+  )
+}
+
+export default Homepage
---
+++ frontend/src/landing_page/signup/Signup.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Signup() {
+  return (
+    <div>
+      <h1>Signup Page</h1>
+    </div>
+  );
+}
+
+// THIS LINE IS LIKELY MISSING OR WRITTEN INCORRECTLY:
+export default Signup;
